d1d5bb3a0f41d48e4c98d90cec90829ed295ddbc
azlux <azlux@outlook.com>
2017-09-26 00:13:37 +0200

    All in one array
    Better when we add new data !

--- index.php
+++ index.php
@@ -1,40 +1,44 @@
+
 <?php
 include("bdd.php");
 
-function average($data,$start,$stop) {
-	$tp = array_slice($data,$start,$stop);
+function average($data,$start,$len) {
+	$tp = array_slice($data,$start,$len);
 	$avg = array_sum($tp)/count($tp);
 	return round($avg);
 }
-function avg_all_data($dataTime,$dataTempCpu,$dataPing1,$dataLoadCpu,$delta) {
-	$first_pos = 0;
+
+function avg_data_delta($dataTime,$data,$delta) {
 	$avg_dataTime = array();
-	$avg_dataTempCpu = array();
-	$avg_dataPing1 = array();
-	$avg_dataLoadCpu = array();
+    $avg_data = array();
 	
+    $first_pos = 0;
 	for ($i = 0; $i < count($dataTime); $i++) {
-		if (abs($dataTime[$i]-$dataTime[$first_pos]) > $delta) {
-			array_push($avg_dataTime, average($dataTime, $first_pos, $i+1));
-			array_push($avg_dataTempCpu, average($dataTempCpu, $first_pos, $i+1));
-			array_push($avg_dataPing1, average($dataPing1, $first_pos, $i+1));
-			array_push($avg_dataLoadCpu, average($dataLoadCpu, $first_pos, $i+1));
-			$first_pos = $i+1;
+ 		if ((abs($dataTime[$i]-$dataTime[$first_pos])) > ($delta*1000)) {
+            array_push($avg_dataTime, average($dataTime, $first_pos, $i-$first_pos));
+			array_push($avg_data, average($data, $first_pos, $i-$first_pos));
+            
+            $first_pos = $i+1;
 		}
 	}
-	return array($avg_dataTime,$avg_dataTempCpu,$avg_dataPing1,$avg_dataLoadCpu);
+	return array($avg_data);
 }
+
+$xlabel = "hour";
 
 if (!empty($_POST['range']) && $_POST['range'] != "day") {
 	if ($_POST['range'] == "week") {
 		$req = $bdd->query('SELECT * FROM data WHERE date > NOW() - INTERVAL 1 WEEK');
-	}
+	        $xlabel = "day";
+        }
 	elseif ($_POST['range'] == "month") {
 		$req = $bdd->query('SELECT * FROM data WHERE date > NOW() - INTERVAL 1 MONTH');
-	}
+	        $xlabel = "week";
+        }
 	elseif ($_POST['range'] == "year") {
 		$req = $bdd->query('SELECT * FROM data WHERE date > NOW() - INTERVAL 1 YEAR');
-	}
+	        $xlabel = "month";
+        }
 	else {
 		exit("LoL Nope");
 	}
@@ -49,26 +53,35 @@ $diskTotal = exec("df -h | grep /dev/root | awk '/[0-9]/ {print $5}'| sed 's/%//
 $diskLog = exec("df -h | grep log2ram | awk '/[0-9]/ {print $5}'| sed 's/%//g'");
 
 $dataTime = array();
-$dataTempCpu = array();
-$dataPing1 = array();
-$dataLoadCpu = array();
+$all_data = array();
 
 # Push every data into an array
 while ($temp = $req->fetch(PDO::FETCH_ASSOC)) {
     array_push($dataTime, floatval(strtotime($temp['date']))*1000);
-    array_push($dataTempCpu, round($temp['temperature_cpu'])/10);
-    array_push($dataPing1, $temp['ping1']);
-    array_push($dataLoadCpu, $temp['load_cpu']);
+    $all_data[0][]= round($temp['temperature_cpu'])/10;
+    $all_data[1][]= round($temp['temperature_room'])/10;
+    $all_data[2][]= $temp['ping1'];
+    $all_data[3][]= $temp['ping2'];
+    $all_data[4][]= $temp['load_cpu'];
+    $all_data[5][]= $temp['ram'];
 }
 
 /* Average for week/month/year data*/
-if (!empty($_POST['range']) && $_POST['range'] != "day") {
-	if ($_POST['range'] == "month") {
-		list($dataTime,$dataTempCpu,$dataPing1,$dataLoadCpu) = avg_all_data($dataTime,$dataTempCpu,$dataPing1,$dataLoadCpu,18000);
-	}
-	elseif ($_POST['range'] == "year") {
-		list($dataTime,$dataTempCpu,$dataPing1,$dataLoadCpu) = avg_all_data($dataTime,$dataTempCpu,$dataPing1,$dataLoadCpu,86400);
-	}
+if (!empty($_POST['range'])) {
+    if ($_POST['range'] == "month" && $_POST['range'] == "year") {
+        if ($_POST['range'] == "month") {
+            $delta = 5*60*60;
+        }
+        elseif ($_POST['range'] == "year") {
+            $delta = 24*60*60;
+        }
+        $dataTime=avg_data_delta($dataTime,$dataTime,$delta);
+        $i=0;
+        foreach ($all_data as $data) {
+            $all_data[$i]=avg_data_delta($dataTime,$data,$delta);
+            $i++;
+        }
+    }
 }
 
 
@@ -76,12 +89,12 @@ if (!empty($_POST['range']) && $_POST['range'] != "day") {
 
 <html>
 <head>
-	<title>Sensors Azlux's Rasp</title>
-	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-	<meta http-equiv="Pragma" content="no-cache" />
-	<meta http-equiv="Expires" content="0" />
+    <title>Sensors Azlux's Rasp</title>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <link rel="stylesheet" href="css/morris.css">
-	<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
@@ -119,51 +132,59 @@ if (!empty($_POST['range']) && $_POST['range'] != "day") {
 		<input type="submit">
 		</form>
 	</div>
-    <header><h3>CPU temperature</h3></header>
+    <header><h3>Temperature</h3></header>
     <div id="temp" style="height: 250px;"></div>
-    <header><h3>Ping Xéfir</h3></header>
-    <div id="ping1" style="height: 250px;"></div>
+    <header><h3>Ping</h3></header>
+    <div id="ping" style="height: 250px;"></div>
     <header><h3>CPU Load (5min)</h3></header>
     <div id="loadCpu" style="height: 250px;"></div>
     <script>
+    
     new Morris.Line({
         element: 'temp',
         data:[<?php
-				for ($i = 0; $i<count($dataTime); $i++) {
-					echo '{ time: ' . $dataTime[$i] . ', value:' . $dataTempCpu[$i] . ' },';
-				}
-			?>],
+	    for ($i = 0; $i<count($dataTime); $i++) {
+		echo '{ time: ' . $dataTime[$i] . ', value1:' . $all_data[0][$i] .', value2:' . $all_data[1][$i] . ' },';
+            }
+	?>],
         xkey: 'time',
-        ykeys: ['value'],
-        labels: ['température'],
+        ykeys: ['value1','value2'],
+        labels: ['CPU','Room'],
         hideHover:true,
-		pointSize:1
+        pointSize:1,
+        yLabelFormat : function (y) { return y.toString() + '°C'; },
+        xLabels:'<?php echo $xlabel ?>'
         });
+        
     new Morris.Line({
-        element: 'ping1',
+        element: 'ping',
         data:[<?php
-				for ($i = 0; $i<count($dataTime); $i++) {
-					echo '{ time: ' . $dataTime[$i] . ', value:' . $dataPing1[$i] . ' },';
-				}
-			?>],
+	    for ($i = 0; $i<count($dataTime); $i++) {
+		echo '{ time: ' . $dataTime[$i] . ', value1:' . $all_data[2][$i].', value2:' . $all_data[3][$i] . ' },';
+	    }
+	?>],
         xkey: 'time',
-        ykeys: ['value'],
-        labels: ['ping'],
+        ykeys: ['value1','value2'],
+        labels: ['crystalyx','linuxgaming'],
         hideHover:true,
-		pointSize:1
+        pointSize:1,
+        yLabelFormat : function (y) { return y.toString() + ' ms'; },
+        xLabels:'<?php echo $xlabel ?>'
         });
+        
     new Morris.Line({
         element: 'loadCpu',
         data:[<?php
-				for ($i = 0; $i<count($dataTime); $i++) {
-					echo '{ time: ' . $dataTime[$i] . ', value:' . $dataLoadCpu[$i] . ' },';
-				}
-			?>],
+	    for ($i = 0; $i<count($dataTime); $i++) {
+		echo '{ time: ' . $dataTime[$i] . ', value1:' . $all_data[4][$i] . ', value2:' . $all_data[5][$i] . ' },';
+	    }
+	?>],
         xkey: 'time',
-        ykeys: ['value'],
-        labels: ['Load'],
+        ykeys: ['value1','value2'],
+        labels: ['Load','Ram'],
         hideHover:true,
-		pointSize:1
+        pointSize:1,
+        xLabels:'<?php echo $xlabel ?>'
         });
 
     </script>
--- sensors.sh
+++ sensors.sh
@@ -1,36 +1,62 @@
 #! /bin/bash
 
-# Temperature of my CPU
-cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp |awk '{printf("%0.f",$1/100)}')
+declare -A values
 
-# Ping my mumble server
-ping1=$(ping -c 1 62.210.107.194 | grep -Po 'time=\K[\d.]+')
+ram_free=$(cat /proc/meminfo | grep  MemAvailable | awk '/[0-9]/ {print $2}')
+ram_total=$(cat /proc/meminfo | grep  MemTotal | awk '/[0-9]/ {print $2}')
 
-# The CPU load (average 5min)
-load_cpu=$(cat /proc/loadavg | awk '/[0-9]/ {print $2*100}')
+values=( ["temperature_cpu"]=$(cat /sys/class/thermal/thermal_zone0/temp |awk '{printf("%0.f",$1/100)}')
+        ["temperature_room"]=$(sudo /home/dmichel/script/thermometer.py | awk '{printf("%0.f",$1*10)}')
+        ["ping1"]=$(ping -c 1 <FQDN> | grep -Po 'time=\K[\d.]+')
+        ["ping2"]=$(ping -c 1 <FQDN> | grep -Po 'time=\K[\d.]+')
+        ["load_cpu"]=$(cat /proc/loadavg | awk '/[0-9]/ {print $2*100}')
+        ["ram"]=$(((($ram_total - $ram_free)*100/$ram_total))) )
+
+
+query="INSERT INTO data_one_day(date"
+for name in "${!values[@]}"; do
+    query+=","
+    query+=$name
+done
+query+=") VALUES (NOW()"
+for name in "${!values[@]}"; do
+    query+=",'"
+    query+=${values[$name]}
+    query+="'"
+done
+query+=")"
 
 # Add thoses data into the "One Day Data" table
-mysql -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -D sensors -e "INSERT INTO data_one_day(date,temperature_cpu,ping1,load_cpu) VALUES (NOW(), $cpu_temp,$ping1,$load_cpu)"
+mysql -u sensors -p************ -D sensors -e "$query"
 
-
+sleep 1
 # Test if there are data older than 1 day+1hour
-t=$(mysql -ss -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -D sensors -e "SELECT count(*) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY - INTERVAL 1 HOUR")
+t=$(mysql -ss -u sensors -p************ -D sensors -e "SELECT count(*) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY - INTERVAL 1 HOUR")
 
 if [ $t != 0 ];then
-	# Calculate the avrage of the hour (from data older than 1day)
-	tp_avg_date=$(mysql -ss -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -ss -D sensors -e "SELECT AVG(UNIX_TIMESTAMP(date)) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
-	tp_avg_tmp_cpu=$(mysql -ss -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -ss -D sensors -e "SELECT AVG(temperature_cpu) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
-	tp_avg_ping1=$(mysql -ss -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -ss -D sensors -e "SELECT AVG(ping1) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
-	tp_avg_load_cpu=$(mysql -ss -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -ss -D sensors -e "SELECT AVG(load_cpu) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
+        # Calculate the average of the hour (from data older than 1day)
+        tp_avg_date=$(mysql -ss -u sensors -p************ -ss -D sensors -e "SELECT AVG(date) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
+        avg_date=${tp_avg_date%.*}
 	
-	# transform it into integer
-	avg_date=${tp_avg_date%.*}
-	avg_tmp_cpu=${tp_avg_tmp_cpu%.*}
-	avg_ping1=${tp_avg_ping1%.*}
-	avg_load_cpu=${tp_avg_load_cpu%.*}
-	
+        query="INSERT INTO data(date"
+
+        for name in "${!values[@]}"; do
+            query+=","
+            query+=$name
+        done
+
+        query+=") VALUES (FROM_UNIXTIME('$avg_date')"
+        
+        for name in "${!values[@]}"; do
+            query+=",'"
+            tp=$(mysql -ss -u sensors -p************ -ss -D sensors -e "SELECT AVG(${values[$name]}) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
+            query+=${tp%.*}
+            query+="'"
+        done
+
 	# Add the data into the database	
-	mysql -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -D sensors -e "INSERT INTO data(date,temperature_cpu,ping1,load_cpu) VALUES (FROM_UNIXTIME($avg_date),$avg_tmp_cpu,$avg_ping1,$avg_load_cpu)"
-	# Rmove the old data from the "One Day Data" table
-	mysql -u sensors -pth61r5h48tr1h5g1fdhb64ht8jngzsq1e6r5 -ss -D sensors -e "DELETE FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY"
+	mysql -u sensors -p************ -D sensors -e "$query"
+	#Remove the old data from the "One Day Data" table
+	mysql -u sensors -p************ -ss -D sensors -e "DELETE FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY"
+
 fi
