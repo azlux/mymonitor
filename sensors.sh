ecbd629b5b8e457d45d9ff6b0c50205197fcbe36
azlux <azlux@outlook.com>
2017-10-19 00:19:20 +0200

    Fix for average

--- sensors.sh
+++ sensors.sh
@@ -10,7 +10,7 @@ values=( ["temperature_cpu"]=$(cat /sys/class/thermal/thermal_zone0/temp |awk '{
         ["ping1"]=$(ping -c 1 <FQDN> | grep -Po 'time=\K[\d.]+')
         ["ping2"]=$(ping -c 1 <FQDN> | grep -Po 'time=\K[\d.]+')
         ["load_cpu"]=$(cat /proc/loadavg | awk '/[0-9]/ {print $2*100}')
-        ["ram"]=$(((($ram_total - $ram_free)*100/$ram_total))) )
+        ["ram"]=$((($ram_total - $ram_free)*100/$ram_total)) )
 
 
 query="INSERT INTO data_one_day(date"
@@ -37,7 +37,7 @@ if [ $t != 0 ];then
         # Calculate the average of the hour (from data older than 1day)
         tp_avg_date=$(mysql -ss -u sensors -p************ -ss -D sensors -e "SELECT AVG(UNIX_TIMESTAMP(date)) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
         avg_date=${tp_avg_date%.*}
-	
+
         query="INSERT INTO data(date"
 
         for name in "${!values[@]}"; do
@@ -46,17 +46,18 @@ if [ $t != 0 ];then
         done
 
         query+=") VALUES (FROM_UNIXTIME('$avg_date')"
-        
+
         for name in "${!values[@]}"; do
             query+=",'"
-            tp=$(mysql -ss -u sensors -p************ -ss -D sensors -e "SELECT AVG(${values[$name]}) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
+            tp=$(mysql -ss -u sensors -p************ -ss -D sensors -e "SELECT AVG($name) FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY")
             query+=${tp%.*}
             query+="'"
         done
+        query+=")"
 
-	# Add the data into the database	
-	mysql -u sensors -p************ -D sensors -e "$query"
-	#Remove the old data from the "One Day Data" table
-	mysql -u sensors -p************ -ss -D sensors -e "DELETE FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY"
+        # Add the data into the database
+        mysql -u sensors -p************ -D sensors -e "$query"
+        #Remove the old data from the "One Day Data" table
+        mysql -u sensors -p************ -ss -D sensors -e "DELETE FROM data_one_day WHERE date < NOW() - INTERVAL 1 DAY"
 
 fi
